0dc444b4b079c7632c651e705515af1c15670dc0
chayongbin <teddy418@nhnnext.org>
2014-12-02 12:11:15 +0900

    project file init

---
+++ NEXT_Library/InitProfileViewController.m
@@ -0,0 +1,37 @@
+//
+//  InitProfileViewController.m
+//  NEXT_Library
+//
+//  Created by 차용빈 on 2014. 12. 2..
+//  Copyright (c) 2014년 FineApple. All rights reserved.
+//
+
+#import "InitProfileViewController.h"
+
+@interface InitProfileViewController ()
+
+@end
+
+@implementation InitProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
