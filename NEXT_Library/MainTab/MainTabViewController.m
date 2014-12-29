06f4caa465fa88cdcd7116e46ba6fd66788a0c79
YD <zootyducky@gmail.com>
2014-12-30 07:01:18 +0900

    main tabbar - tabbaritem image 색깔 회색으로 변하는 것 수정, select 했을때 icon이 나타지 않음...

--- NEXT_Library/MainTab/MainTabViewController.m
+++ NEXT_Library/MainTab/MainTabViewController.m
@@ -10,4 +10,23 @@
 
 @implementation MainTabViewController
 
++ (void)initialize
+{
+    //the color for the text for unselected tabs
+    [UITabBarItem.appearance setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor redColor]} forState:UIControlStateNormal];
+    
+    //the color for selected icon
+    [[UITabBar appearance] setSelectedImageTintColor:[UIColor whiteColor]];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7) {
+        for (UITabBarItem *tbi in self.tabBar.items) {
+            tbi.image = [tbi.image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+        }
+    }
+}
 @end
